55e9a57cff7dd652f20159b91f33015b47de11ad
vidhun <vidhun.v@eyerov.com>
2025-08-27 13:00:22 +0530

    small changes

--- obstacle_mover/obstacle_mover/obstacle_mover.py
+++ obstacle_mover/obstacle_mover/obstacle_mover.py
@@ -8,20 +8,26 @@ class ObstacleMover(Node):
 
     def __init__(self):
         super().__init__('Obstacle_Mover')
+        # Publisher for obstacle movement commands
         self.obstacle_pub = self.create_publisher(Twist, '/moving_box/cmd_vel', 10)
+       
+        # Subscriber for obstacle location
         self.obstacle_odom_sub = self.create_subscription(Odometry, '/moving_box/odom', self.odom_callback, 10)
+       
         self.factor = 1.0
-        self.x = 0
+        self.y = 0
+       
+        # Timer for obstacle movement
         self.timer_ = self.create_timer(0.01, self.obstacle_mover)
         
     def odom_callback(self, msg):
-        self.x = msg.pose.pose.position.y
+        self.y = msg.pose.pose.position.y
     
     def obstacle_mover(self):
         twist_msg = Twist()
-        if 0 < self.x < 1.5:
+        if 0 < self.y < 1.5:
             self.factor = -1.0
-        elif -4.0 > self.x > -4.5:
+        elif -4.0 > self.y > -4.5:
             self.factor = 1.0
         twist_msg.linear.y = self.factor * 0.5
         self.obstacle_pub.publish(twist_msg)
--- simplebot_description/launch/gazebo.launch.py
+++ simplebot_description/launch/gazebo.launch.py
@@ -1,3 +1,5 @@
+# Launch file for starting Gazebo with the simple bot
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
@@ -15,6 +17,7 @@ def generate_launch_description():
     ekf_config = os.path.join(pkg_dir, 'config', 'ekf.yaml')
     world_path = os.path.join(pkg_dir, "world", "grid.world")
     
+    # Gazebo launch file
     gazebo_node = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [os.path.join(
@@ -26,6 +29,7 @@ def generate_launch_description():
         launch_arguments= {'world': world_path}.items()
     )
     
+    # Robot State Publisher Node
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -34,23 +38,16 @@ def generate_launch_description():
             'robot_description': Command(['xacro ', urdf_path])
         }]
     )
-    
-    joint_state_publisher_node = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name= 'joint_state_publisher',
-        parameters=[{
-            'use_sim_time': True,
-            'robot_description': Command(['xacro ', urdf_path])
-        }]
-    )
+
+    # Spawn Robot
     spawn_robot = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
         arguments=['-entity', 'simple_bot', '-topic', 'robot_description'],
         output='screen'
     )
-    
+
+    # Robot Localization Node
     robot_localization_node = Node(
         package= 'robot_localization',
         executable='ekf_node',
@@ -61,7 +58,6 @@ def generate_launch_description():
     
     return LaunchDescription([
         gazebo_node,
-        # joint_state_publisher_node,
         robot_state_publisher_node,
         spawn_robot,
         robot_localization_node
